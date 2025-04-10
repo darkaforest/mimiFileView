89967466f72ef8dad14d28fb6395b9682c272182
darkaforest <darkaforest@gmail.com>
2025-04-11 02:17:14 +0800

    feat: 后端跨域配置

--- server/src/main/java/org/darkaforest/config/WebConfig.java
+++ server/src/main/java/org/darkaforest/config/WebConfig.java
@@ -7,6 +7,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -29,6 +30,15 @@ public class WebConfig implements WebMvcConfigurer {
         String filePath = ConfigConstants.getFileDir();
         LOGGER.info("Add resource locations: {}", filePath);
         registry.addResourceHandler("/**").addResourceLocations("classpath:/META-INF/resources/","classpath:/resources/","classpath:/static/","classpath:/public/","file:" + filePath);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")//项目中的所有接口都支持跨域
+                .allowedOriginPatterns("*")//所有地址都可以访问，也可以配置具体地址
+                .allowCredentials(true)
+                .allowedMethods("*")//"GET", "HEAD", "POST", "PUT", "DELETE", "OPTIONS"
+                .maxAge(3600);// 跨域允许时间
     }
 
 
